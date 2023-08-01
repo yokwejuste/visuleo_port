e1aa68ea82078f6dfbabc587089a12466f5fc132
Steve Yonkeu <yokwejuste@yahoo.com>
2023-08-01 06:39:03 +0100

    feat: implements tenant configuration (#20)

---
+++ apps/portfolio/paginators/projects_pagination.py
@@ -0,0 +1,6 @@
+from rest_framework.pagination import PageNumberPagination
+
+class ProjectsPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
