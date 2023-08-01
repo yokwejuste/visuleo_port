a808b5da402e1c8cd2cb86dc15d5e1b73af32b49
Steve Yonkeu <yokwejuste@yahoo.com>
2023-08-01 06:37:22 +0100

    feat: implements tenant configuration

---
+++ apps/portfolio/views/projects_view.py
@@ -0,0 +1,56 @@
+from rest_framework.filters import SearchFilter
+from rest_framework import permissions
+from rest_framework.viewsets import ModelViewSet
+from models import Projects, Categories
+from serializers import ProjectsSerializer, ProjectsResponseSerializer
+from drf_yasg.utils import swagger_auto_schema
+from utils import load_document
+from paginators import ProjectsPagination
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+
+
+class ProjectsViewSet(ModelViewSet):
+    queryset = Projects.objects.all()
+    serializer_class = ProjectsSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [SearchFilter]
+    pagination_class = ProjectsPagination
+    parser_classes = [MultiPartParser, FormParser]
+    filterset_fields = ["category", "tags", "is_published"]
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Projects.objects.all()
+        else:
+            return Projects.objects.filter(is_featured=True)
+
+    @swagger_auto_schema(
+        operation_id="Get all projects",
+        operation_summary="Get all projects",
+        operation_description=load_document("portfolio/projects_docs.html"),
+    )
+    def list(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+        search = request.query_params.get("search")
+        if search is not None:
+            queryset = queryset.filter(name__icontains=search)
+        id = request.query_params.get("id")
+        if id is not None:
+            queryset = queryset.filter(id=id)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = ProjectsResponseSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = ProjectsSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        categories = request.data.pop("categories")
+        for category in categories:
+            category, _ = Categories.objects.get_or_create(**category)
+            Categories.objects.get_or_create(**category)
+        serializer = ProjectsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
