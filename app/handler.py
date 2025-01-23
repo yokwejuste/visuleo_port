8ef2926a7b3c63817c7b9cfd93f584d510c76742
Steve Yonkeu <yokwejuste@yahoo.com>
2025-01-23 01:16:33 +0100

    splitting dependencies

--- app/handler.py
+++ app/handler.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from mangum import Mangum
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "visuleo_port.settings")
 
 application = get_wsgi_application()
 handler = Mangum(application)
